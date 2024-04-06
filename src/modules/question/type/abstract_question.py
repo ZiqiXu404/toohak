d0aa60070f4c7fb20ea8aed3c9ad238c19162c26
nickbar01234 <nickbar01234@gmail.com>
2024-04-06 01:49:16 -0400

    Remove AbstractSerializable

--- src/modules/question/type/abstract_question.py
+++ src/modules/question/type/abstract_question.py
@@ -1,10 +1,9 @@
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 import pygame
-from ...serializable.abstract_serializable import AbstractSerializable
 from ... import AbstractSolution
 
 
-class AbstractQuestion(AbstractSerializable):
+class AbstractQuestion(ABC):
     def __init__(self, question: str, solution: AbstractSolution):
         self.__question = question
         self.__solution = solution
@@ -19,12 +18,6 @@ class AbstractQuestion(AbstractSerializable):
     def draw(self, game: pygame):
         '''
         Given a pygame instance, represent the question on the canvas.
-        '''
-
-    @abstractmethod
-    def serialize(self) -> bytes:
-        '''
-        Convert instance to bytecode.
         '''
 
     def get_question(self):
